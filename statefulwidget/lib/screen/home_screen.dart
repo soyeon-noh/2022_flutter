c12f4cb83165bb824d9ab11ed243e18a93af2859
soyeon-noh <1010qwerty12@gmail.com>
2022-10-15 22:20:40 +0900

    stateful widget

---
+++ statefulwidget/lib/screen/home_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+// statefulWidget
+class HomeScreen extends StatefulWidget {
+  final Color color;
+
+  const HomeScreen({
+    required this.color,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeScreenState();
+  }
+
+  // create State
+
+}
+
+// state는 재사용하는데 constructure는 한번만 불리기때문에 생성자로 color를 지정하면 안된다.
+// wiget이라는 키워드를 사용하여 statefulWidget의 요소를 가져올 수 있다.
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      color: widget.color,
+    );
+  }
+}
+
+// statelessWidget
+// 생성자에 color를 지정하여 사용
+class _HomeScreen extends StatelessWidget {
+  final Color color;
+
+  const _HomeScreen({
+    required this.color,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      color: color,
+    );
+  }
+}
