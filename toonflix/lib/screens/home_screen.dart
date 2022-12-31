612f496392139fb30f007773222bd419d95a53e3
soyeon-noh <1010qwerty12@gmail.com>
2022-12-31 21:58:42 +0900

    toonflix

---
+++ toonflix/lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        elevation: 2,
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
